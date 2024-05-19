d0f666fd4ae812d5ad67d6a19154c215ac5985d3
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-05-19 17:32:50 +0900

    for archive

--- project/project/static/javaScript/practice.js
+++ project/project/static/javaScript/practice.js
@@ -1,17 +1,23 @@
 // 아래에 코드를 작성해주세요
 
 // ============== 주문서 접수 버튼 선택 ============== 
-
-
+const complete = document.getElementById('complete');
 
 // ============== 이벤트 핸들러 작성 ============== 
+const handleSubmit = () => {
+  // 재료 선택 & 값 변경
+  const radiobox = document.querySelectorAll("input[type='radio'][name='food']:checked");
+  document.getElementById('result1').textContent = radiobox[0].value;
+  console.log(radiobox);
 
+  // 맵기 선택 & 값 변경
+  const select1 = document.getElementById("spicy").value;
+  document.getElementById('result2').textContent = select1;
 
-
-
-
-
+  // 얼얼 선택 & 값 변경
+  const select2 = document.getElementById("burning").value;
+  document.getElementById('result3').textContent = select2;
+}
 
 // ============== 이벤트 리스너 연결 ============== 
-
-
+complete.addEventListener('click', handleSubmit);
--- project/project/static/javaScript/selector.js
+++ project/project/static/javaScript/selector.js
@@ -1,8 +1,8 @@
 // ----------------------------------------------------
 // 1번 실습 - Element 찾기
-const element1 = '이 곳을 수정해주세요';
-const element2 = '이 곳을 수정해주세요';
-const element3 = '이 곳을 수정해주세요';
+const element1 = document.getElementById("element1");
+const element2 = document.getElementsByClassName("element2");
+const element3 = document.querySelector("#element3");
 
 // 브라우저에서 출력되는 콘솔 내용을 확인해보세요.
 console.log(element1);
@@ -10,14 +10,24 @@ console.log(element2);
 console.log(element3);
 
 // 아래에서 코드를 완성하여 태그 내 글자를 출력해보세요.
-console.log("element1의 글자 : " + '이 곳을 수정해주세요');
-console.log("element2의 글자 : " + '이 곳을 수정해주세요');
-console.log("element3의 글자 : " + '이 곳을 수정해주세요');
+console.log("element1의 글자 : " + element1.innerText);
+console.log("element2의 글자 : " + element2[0].innerText);
+console.log("element3의 글자 : " + element3.innerText);
 
 // ----------------------------------------------------
 // 2번 실습 - 틀린 단어 찾기
 // 아래에 코드를 작성하세요.
+const dog = document.getElementById("dog");
+const phone = document.getElementById("phone");
+const water = document.getElementById("water");
 
+dog.innerText = "강아지";
+phone.innerText = "휴대폰";
+water.innerText = "물";
+
+console.log(dog);
+console.log(phone);
+console.log(water);
 
 
 
@@ -26,5 +36,15 @@ console.log("element3의 글자 : " + '이 곳을 수정해주세요');
 // 네이버 주소 : https://www.naver.com
 // 아래에 코드를 작성하세요.
 
+const q1 = document.querySelector('.quest1');
+const q2 = document.querySelector('.quest2');
+const q3 = document.querySelector('.quest3');
+const q4 = document.querySelector('.quest4');
+const q5 = document.querySelector('.quest5');
 
 
+q1.style.color = 'red';
+q2.style.visibility = 'hidden';
+q3.style.backgrounColor = 'blue';
+q4.style.display = 'none';
+q5.setAttribute('href', 'https://www.naver.com');
